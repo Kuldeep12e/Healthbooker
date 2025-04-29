074945a56c492469356af745812d72408dd60d2a
Kuldeep <kuldeep0105yadav@gmail.com>
2025-04-29 06:34:07 +0530

    health booker complete

---
+++ client/src/pages/RoomPage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+
+function Roompage() {
+  const myMeeting = async (element) => {
+    const appID = 1141450940;
+    const serverSecret = "da69cb3e49def060a31d3130029dee33";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID, serverSecret, 'ayush', Date.now().toString(), "Ayush"
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: 'Copy Link',
+          url: `http://localhost:3000/room`
+        }
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneOnOneCall,
+      },
+      showScreenSharingButton: false
+    });
+  };
+
+  return (
+    <div>
+      <div ref={myMeeting} style={{ width: '100vw', height: '100vh' }} />
+    </div>
+  );
+}
+
+export default Roompage;
